e98d0f13a2ed8014f6183db1e23422665552a8ef
Clouddark76 <125485577+Clouddark76@users.noreply.github.com>
2026-09-15 07:17:35 -0400

    Update depinfo.sh

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -4,14 +4,14 @@
 # Make sure to keep v_ndk and v_ndk_n in sync, both are listed on the NDK download page
 
 v_sdk=14742923_latest
-v_ndk=r29
-v_ndk_n=29.0.14206865
+v_ndk=r30
+v_ndk_n=30.0.16248370
 v_sdk_platform=36
 v_sdk_build_tools=36.0.0
 
 v_lua=5.2.4
 v_unibreak=7.0
-v_harfbuzz=14.2.1
+v_harfbuzz=14.3.1
 v_fribidi=1.0.16
 v_freetype=2.14.3
 v_mbedtls=3.6.7
